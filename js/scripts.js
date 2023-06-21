0c197f60734e6003afaca0ea478e20dae2ab7011
TieNewin <125307533+TieNewin@users.noreply.github.com>
2023-06-21 16:31:51 -0500

    created array of pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello World');
-let favoriteFood = 'Chicken Alfredo';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: 'grass, poison'},
+    {name: 'Charmander', height: 0.6, types: 'fire'},
+    {name: 'Squirtle', height: 0.5, types: 'water'}
+];
+
