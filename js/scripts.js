497dc29fb987f334ccf46e4225df9802acb9c288
TieNewin <125307533+TieNewin@users.noreply.github.com>
2023-06-22 14:05:29 -0500

    changed script to IIFE and modified forEach loop to use new function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: 'grass/poison'},
-    {name: 'Charmander', height: 0.6, types: 'fire'},
-    {name: 'Squirtle', height: 0.5, types: 'water'}
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Bulbasaur', height: 0.7, types: 'grass/poison'},
+        {name: 'Charmander', height: 0.6, types: 'fire'},
+        {name: 'Squirtle', height: 0.5, types: 'water'}
+    ];
 
-pokemonList.forEach(function(pokemon) {
+     return {
+        add: function(pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function() {
+            return pokemonList;
+        }
+     };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write(pokemon.name + " " + pokemon.height + " " + pokemon.types + "</br>");
 });
-
