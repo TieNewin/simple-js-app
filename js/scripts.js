f617f15b116fd4b327537f532f2c46710192b902
TieNewin <125307533+TieNewin@users.noreply.github.com>
2023-06-22 13:36:30 -0500

    changed for loop to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: 'grass, poison'},
+    {name: 'Bulbasaur', height: 0.7, types: 'grass/poison'},
     {name: 'Charmander', height: 0.6, types: 'fire'},
     {name: 'Squirtle', height: 0.5, types: 'water'}
 ];
 
-for (let x = 0; x < pokemonList.length; x++) {
-    let result = pokemonList[x].height > 0.5 ? "Wow, that's big!" : "";
-    document.write(pokemonList[x].name + " (height: " + pokemonList[x].height + ") " + result + "<br>");
-}
+pokemonList.forEach(function(pokemon) {
+    document.write(pokemon.name + " " + pokemon.height + " " + pokemon.types + "</br>");
+});
 
