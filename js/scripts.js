b627ea03647c36f1d3803c30c1a6a772d7d0c30f
TieNewin <125307533+TieNewin@users.noreply.github.com>
2023-06-23 15:21:50 -0500

    made pokemon names into buttons that log pokemon details to the console when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,40 @@ let pokemonRepository = (function () {
         {name: 'Squirtle', height: 0.5, types: 'water'}
     ];
 
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    function addListItem(pokemon) {
+        let list = document.querySelector('.list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+
+        button.innerText = pokemon.name;
+        button.addEventListener('click', function () {
+            showDetails(pokemon);
+        });
+
+        listItem.appendChild(button);
+        list.appendChild(listItem);
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon.name + ' ' + pokemon.height + ' ' + pokemon.types)
+    }
+
      return {
-        add: function(pokemon) {
-            pokemonList.push(pokemon);
-        },
-        getAll: function() {
-            return pokemonList;
-        }
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails,
      };
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write(pokemon.name + " " + pokemon.height + " " + pokemon.types + "</br>");
+pokemonRepository.getAll().forEach( function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
 });
